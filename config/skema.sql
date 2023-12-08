0d5ff18dc5dc16e6393d2027dc7ae0958557156c
NAJ1D <134794893+NAJ1D@users.noreply.github.com>
2023-12-08 15:08:16 +0700

    Update skema.sql

--- config/skema.sql
+++ config/skema.sql
@@ -1,21 +1,40 @@
-CREATE TABLE IF NOT EXISTS
-'Blue_Horizon';
+CREATE DATABASE IF NOT EXISTS `blue_horizon`;
 
-USE 'Blue_Horizon';
+USE `blue_horizon`;
 
-CREATE TABLE IF NOT EXISTS Room {
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    type VARCHAR (256) NOT NULL,
-    price INT NOT NULL,
-};
+-- CREATE TABLE PRODUCT
 
-CREATE TABLE IF NOT EXISTS Customer {
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nama VARCHAR (256) NOT NULL,
-    address VARCHAR (256) NOT NULL,
-};
+CREATE TABLE IF NOT EXISTS Room (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  type VARCHAR(100) NOT NUL,
+  price INT NOT NULL 
+);
 
-INSERT INTO Room  (type, price) VALUES ('standar', 2500000);
+CREATE TABLE IF NOT EXISTS Customer (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(1000) NOT NULL,
+  addres VARCHAR(1000) NOT NULL,
+  age INT NOT NULL
+);
 
+CREATE TABLE IF NOT EXISTS Reservation (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  id_customer INT NOT NULL,
+  id_room INT NOT NULL,
+  check_in DATE,
+  check_out DATE
+  total INT NOT NULL
+);
 
+-- data seed
+INSERT INTO Room (type, price) VALUES ( 
+    'Standard Room', '250000'
+);
 
+INSERT INTO Customer (name, address, age) VALUES (
+    'Steven Grant', 'Georgia,AS', '45'
+);
+
+INSERT INTO Reservation (id_customer, id_room, check_in, check_out, total) VALUES (
+    '1213131', '2', '2023-11-10', '2023-11-13', '1000000'
+)
