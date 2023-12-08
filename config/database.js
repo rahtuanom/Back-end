580ff3c955908df245d0540ba314ae5a7f91ad2e
NAJ1D <134794893+NAJ1D@users.noreply.github.com>
2023-12-08 15:08:40 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const connectionPool = mysql.createPool({
-    uri: process.env.DB_HPL || 'mysql://root:password@localhost:3306/Blue_Horizon'
+    uri: process.env.DB_URI || 'mysql://root:password@localhost:3306/Blue_Horizon'
 })
 
 
