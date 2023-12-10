c3c9db6bd01b15bfb01b1ef9f48830361be23baf
ekiirisky <ekhiriski@gmail.com>
2023-12-10 20:44:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// const connection = require('./app/model/index')
-const express = require('express');
+// const connection = require("./app/model/index")
+const express = require("express");
 const server = express();
-require ('dotenv').config();
+require("dotenv").config();
 
 const fs = require("fs");
 const cors =  require("cors");
-const { request } = require('http');
-const { type } = require('os');
+const { request } = require("http");
+const { type } = require("os");
 
-const { pemesananRoutes } = require('./routes/pemesanan.routes');
-const { roomRoutes } = require('./routes/room.routes');
-const { messageRoutes } = require('./routes/message.routes'); 
+const { pemesananRoutes } = require("./routes/pemesanan.routes");
+const { roomRoutes } = require("./routes/room.routes");
+const { messageRoutes } = require("./routes/message.routes"); 
 
 const PORT = process.env.PORT || 3000;
 server.use(cors());
@@ -37,5 +37,5 @@ server.all("*", async (req, res) => {
 });
 
 server.listen(PORT, "0.0.0.0", () =>{
-    console.log('iya udah nyala nih, silakan di cek aja di http://localhost:3000/')
+    console.log("iya udah nyala nih, silakan di cek aja di http://localhost:3000/")
 });
