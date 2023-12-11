fa0d89501a287e0ec1038740b721c16aaabcac8c
Rahtu Anom <rahtuanom10@gmail.com>
2023-12-12 02:17:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,37 +4,50 @@ const server = express();
 require("dotenv").config();
 
 const fs = require("fs");
-const cors =  require("cors");
+const cors = require("cors");
 const { request } = require("http");
 const { type } = require("os");
 
-const { pemesananRoutes } = require("./routes/pemesanan.routes").default;
+const { pemesananRoutes } = require("./routes/pemesanan.routes");
 const { roomRoutes } = require("./routes/room.routes");
-const { contactusRoutes } = require("./routes/contactus.routes"); 
+const { contactusRoutes } = require("./routes/contactus.routes");
 
 const PORT = process.env.PORT || 3000;
 server.use(cors());
-server.use(express.urlencoded({extended:true}));
+server.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 server.use(express.json());
 
-server.get("/",(request, response) => {
-    response.send("Aku balikin responmu")
+server.get("/", (request, response) => {
+  response.send("Aku balikin responmu");
 });
 
 server.get("/hello", async (req, res) => {
-	res.send("hello");
+  res.send("hello");
 });
 
 server.use("/pemesanan", pemesananRoutes);
 server.use("/room", roomRoutes);
 server.use("/contactus", contactusRoutes);
 
-server.all("*", async (req, res) => {
-	res.json({
-		contactus: "Routes tidak tersedia"
-	});
+server.all("*", async (req, res, next) => {
+  res.status(404).json({
+    message: "Routes tidak tersedia",
+  });
 });
 
-server.listen(PORT, "0.0.0.0", () =>{
-    console.log("iya udah nyala nih, silakan di cek aja di http://localhost:3000/")
+server.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({
+    message: "Internal server error",
+  });
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(
+    "iya udah nyala nih, silakan di cek aja di http://localhost:3000/"
+  );
 });
