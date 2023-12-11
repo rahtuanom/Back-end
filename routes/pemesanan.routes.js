24392212a68950f7aae9811bd3291ed57e122182
Rahtu Anom <rahtuanom10@gmail.com>
2023-12-12 02:07:20 +0800

    Update pemesanan.routes.js

--- routes/pemesanan.routes.js
+++ routes/pemesanan.routes.js
@@ -1,54 +1,61 @@
 const express = require("express");
-const pemesananRoutes  = express.Router();
-const { prisma } = require("../config/prisma");
+const pemesananRoutes = express.Router();
+const {
+    prisma
+} = require("../config/prisma");
 
 pemesananRoutes.post("/", async (req, res) => {
+    try {
+        console.log("Received form data:", req.body); // Logging untuk memeriksa data yang diterima
 
-  try {
-    const checkInDate = new Date(Date.parse(req.body.check_in));
-    const checkOutDate = new Date(Date.parse(req.body.check_out));
+        const checkInDate = new Date(Date.parse(req.body.checkin));
+        const checkOutDate = new Date(Date.parse(req.body.checkout));
 
-    const newPemesanan = await prisma.pemesanan.create({
-      data: {
-        name: req.body.name,
-        email: req.body.email,
-        check_in: checkInDate.toISOString(),
-        check_out: checkOutDate.toISOString(),
-        adults: req.body.adults,
-        childs: req.body.childs,
-        rooms: req.body.rooms,
-        type_room: req.body.type_room,
-      },
-    });
+        const newPemesanan = await prisma.pemesanan.create({
+            data: {
+                name: req.body.full_name,
+                email: req.body.email,
+                check_in: checkInDate.toISOString(),
+                check_out: checkOutDate.toISOString(),
+                adults: req.body.adults,
+                childs: req.body.childs,
+                rooms: req.body.rooms,
+                type_room: req.body.typeRoom,
+            },
+        });
 
-    res.status(200).json(newPemesanan);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal post server error" });
-  }
+        res.status(200).json(newPemesanan);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: "Internal server error"
+        });
+    }
 });
 
 pemesananRoutes.get("/", async (req, res) => {
-  try {
-    const Pemesanan = await prisma.pemesanan.findMany();
-    if (Pemesanan) {
-      res.status(200).json(Pemesanan);
-    } else {
-      res.status(404).json({ message: "Pemesanan not found" });
+    try {
+        const Pemesanan = await prisma.pemesanan.findMany();
+        if (Pemesanan) {
+            res.status(200).json(Pemesanan);
+        } else {
+            res.status(404).json({
+                message: "Pemesanan not found"
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: "Internal server error"
+        });
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal get server error" });
-  }
 });
 
- 
-  //
+//
 pemesananRoutes.get("/", async (req, res) => {
     const pemesanan = await prisma.pemesanan.findMany();
     res.status(200).send(pemesanan);
 });
-
 
 pemesananRoutes.get("/:id", async (req, res) => {
     const pemesanan = await prisma.pemesanan.findUnique({
@@ -57,10 +64,12 @@ pemesananRoutes.get("/:id", async (req, res) => {
         },
     });
     if (!pemesanan)
-    res.status(404).json({
-        message: "pemesanan tidak tersedia",
-    });
+        res.status(404).json({
+            message: "pemesanan tidak tersedia",
+        });
     else res.status(200).json(pemesanan);
 });
 
-module.exports = { pemesananRoutes };
+module.exports = {
+    pemesananRoutes
+};
