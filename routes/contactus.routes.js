b7f7b3ee51ac3ac308783a385d1a4ccd4ae6183d
ekiirisky <ekhiriski@gmail.com>
2023-12-11 08:27:57 +0700

    up

--- routes/contactus.routes.js
+++ routes/contactus.routes.js
@@ -1,24 +1,53 @@
+// const express = require("express");
+// const contactusRoutes = express.Router();
+// const { prisma } = require("../config/prisma");
+
+// // get all contactus
+// contactusRoutes.get("/", async (req, res) => {
+// 	const contactus = await prisma.contactus.findMany();
+// 	res.status(200).send(contactus);
+// });
+
+
+// contactusRoutes.post("/", async (req, res) => {
+//     const { firstName, lastName, email, mobileNumber, message, cratedAt } = req.body;
+//     const newContactus = await prisma.contactus.create({
+//         data: {
+//             firstName: firstName,
+//             lastName: lastName,
+//             email: email,
+//             mobileNumber: mobileNumber,
+//             message: message,
+//             cratedAt: cratedAt
+//         },
+//     });
+//     res.status(201).json({
+//         contactus: "contactus created",
+//         data: newContactus,
+//     });
+// });
+
+// module.exports = { contactusRoutes };
+
 const express = require("express");
 const contactusRoutes = express.Router();
 const { prisma } = require("../config/prisma");
 
 // get all contactus
 contactusRoutes.get("/", async (req, res) => {
-	const contactus = await prisma.contactus.findMany();
-	res.status(200).send(contactus);
+    const contactus = await prisma.contactus.findMany();
+    res.status(200).send(contactus);
 });
 
-
 contactusRoutes.post("/", async (req, res) => {
-    const { firstName, lastName, email, mobileNumber, message, cratedAt } = req.body;
+    const { firstName, lastName, email, message, createdAt } = req.body; // Removed mobileNumber from destructuring
     const newContactus = await prisma.contactus.create({
         data: {
             firstName: firstName,
             lastName: lastName,
             email: email,
-            mobileNumber: mobileNumber,
             message: message,
-            cratedAt: cratedAt
+            createdAt: createdAt, // Corrected the field name to createdAt
         },
     });
     res.status(201).json({
